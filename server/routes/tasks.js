47b2c47212307574b8b33cba806b90c55847b733
Hack Reactor Student <student@hackreactor.com>
2018-07-18 13:58:26 -0500

    Refactor server to use router files for endpoints

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const controller = require('../controller');
 const router = express.Router();
+
 
 router.post('/', (req, res)=>{
     console.log('adding task')
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const controller = require('../controller');
 const router = express.Router();
 
 router.post('/', (req, res)=>{
