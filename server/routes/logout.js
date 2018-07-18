584988f363cb7a2085373c313c39851e9ca088c3
Scott <scottgrimes89@gmail.com>
2018-07-18 16:35:25 -0500

    Add airbnb style

--- server/routes/logout.js
+++ server/routes/logout.js
@@ -1,10 +1,11 @@
 const express = require('express');
+
 const router = express.Router();
 
-router.get('/', (req, res)=>{
-    console.log('logging out user');
-    req.logout();
-    res.redirect('/');
-})
+router.get('/', (req, res) => {
+  console.log('logging out user');
+  req.logout();
+  res.redirect('/');
+});
 
 module.exports = router;
