7bbdda14271e836b1999c0f08ea6ec680b38ed42
athousandleaves <superamare@gmail.com>
2018-07-19 11:41:27 -0500

    Add hashing for user registration

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const controller = require('../controller');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
-router.post('/', (req, res)=>{
+router.post('/', (req, res) => {
     console.log('adding user')
-    controller.addUser(req.body)
-        .then((result) => { console.log("success"); return res.send(result) })
-        .catch((err) => { console.log(err); return res.send(false) });
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            controller.addUser({ username: req.body.username, password: hash })
+                .then((result) => { console.log("success"); return res.send(result) })
+                .catch((err) => { console.log(err); return res.send(false) });
+        });
 });
 
 router.get('/', (req, res)=>{
