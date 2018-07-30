b7f7fde5b1a5bfbf5721a25b3361b696cb7bbb8d
Scott <scottgrimes89@gmail.com>
2018-07-30 11:20:11 -0500

    Add form to allow user to update password

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,25 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import UpdateUserForm from './UpdateUserForm.jsx';
 
 const Home = ({ user }) => {
   const splashStyle = {
-    maxWidth: "300",
-    fontSize: "2em",
-    fontWeight: "lighter",
-    margin: "4em auto",
-    textAlign: "center"
+    maxWidth: '300',
+    fontSize: '2em',
+    fontWeight: 'lighter',
+    margin: '4em auto',
+    textAlign: 'center',
   };
   if (user) {
     return (
       <div style={splashStyle}>
         Welcome,
-        <span style={{ color: "grey" }}> {user.username}</span>
+        <span style={{ color: 'grey' }}>
+          {' '}
+          {user.username}
+        </span>
         .
         <br />
-        Create a 
-        <Link to="/addsprint" style={{color: '#ed1a5c', textDecoration: 'none'}}> new sprint</Link>
+        Create a
+        <Link to="/addsprint" style={{ color: '#ed1a5c', textDecoration: 'none' }}>
+          {' '}
+          new sprint
+        </Link>
         , or view a sprint from the dropdown at the top of the screen.
+        <div>
+          Update Password
+          <UpdateUserForm user={user} />
+        </div>
       </div>
+
     );
   }
   return (
