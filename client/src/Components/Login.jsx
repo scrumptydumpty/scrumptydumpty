016f1fada218a9417369deb3bc0123d8bc44c2c6
Jeffrey Chea <jeffreychea234@gmail.com>
2018-07-31 14:08:55 -0500

    change login and signup pages and add an image

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-
+import logo from './scrumlords.png';
 const api = require('../api');
 
 class Login extends React.Component {
@@ -48,7 +48,8 @@ class Login extends React.Component {
 
   render() {
     return (
-      <div style={{ textAlign: 'center', height: '600px', width: '400px', border: '1px solid red'}}>
+      <div style={{ margin: 'auto', marginTop: '10%', textAlign: 'center', height: '600px', width: '400px', border: '1px solid red'}}>
+        <img src={logo} style={{width: '100%', padding: '50px 0'}} />
         <form onSubmit={this.handleSubmit}>
           <div>
             <TextField required id="username" label="Username" value={this.state.username} margin="normal" onChange={this.handleUsernameChange} />
@@ -66,8 +67,6 @@ class Login extends React.Component {
             </Button>
           </div>
         </form>
-
-
       </div>
     );
   }
